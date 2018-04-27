9d5195536c2a0cfb82ce3434ea495be66eda0056
Or Sharbat <or@200apps.co>
2018-04-27 05:40:26 +0300

    hi

--- product.js
+++ product.js
@@ -20,21 +20,20 @@ async function addProduct(name, desc, image, userId){
 async function getAllProducts(){
     const query = server.datastore.createQuery(kinds.product)
     let products = await server.datastore.runQuery(query)
-    console.log(products)
-    return products[0].map(product => {
+    products[0].map(product => {
         product.id = product[server.datastore.KEY].id
         return product
     })
+    return products
 }
 
 async function addLike(body){
     let x = await server.datastore.get(server.datastore.key([kinds.product, parseInt(body.productId)]))
     x = x[0]
-    console.log(x.likes)
     if (x.likes.indexOf(body.userId)<0) {
         x.likes.push(body.userId)
+        server.datastore.save(x)
     }
-    server.datastore.save(x)
     ownerProductId = x.userId
     // ownerProduct =  await server.datastore.get(server.datastore.key([kinds.user, parseInt(ownerProductId)]))
     return await checkMatch(body.userId, ownerProductId)
